6322d196fdc2590c4a96510cb6fe457781ccff2b
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-03-08 17:23:03 +0700

    Praktikum 2.2 - Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,23 +1,63 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+    public function index()
+    {
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data'=>$user]);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data'=>$user]);
+
+        // $user = UserModel::findOr(
+        //     20,
+        //     ['username', 'nama'],
+        //     function () {
+        //         abort(404);
+        //     }
+        // );
+        // return view('user', ['data' => $user]);
+
+    // $user = UserModel::findOr(
+    //     1,
+        //     ['username', 'nama'],
+        //     function () {
+        //         abort(404);
+        //     }
+        // );
+
+        // return view('user', ['data' => $user]);
+
+
+
+        // $user = UserModel::firstwhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        //Praktikum-1
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
 
 
         // update data user dengan Eloquent Model
@@ -41,4 +81,3 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
     }
 }
-
