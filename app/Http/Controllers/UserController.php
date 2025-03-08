11b7bf18efda925d7ecdd63accd2cb7310b89ddb
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-03-08 23:40:03 +0700

    Praktikum 2.3 - 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,82 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data'=>$user]);
 
+        $user = UserModel::create([
+            'username' => 'managerll',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+        $user->username = 'manager12';
+        $user->save();
+        $user->wasChanged(); // true 
+        $user->wasChanged('username'); // true 
+        $user->wasChanged(['username', 'level_id']); // true 
+        $user->wasChanged('nama'); // false 
+        $user->wasChanged(['nama', 'username']); // true
+
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ]);
+        // $user->username = 'manager56';
+
+        // $user->isDirty(); // true 
+        // $user->isDirty('username'); // true 
+        // $user->isDirty('nama'); // false 
+        // $user->isDirty(['nama', 'username']); // true 
+
+        // $user->isClean(); // false 
+        // $user->isClean('username'); // false 
+        // $user->isClean('nama'); // true 
+        // $user->isClean(['nama', 'username']); // false 
+
+        // $user->save();
+
+        // $user->isDirty(); // false 
+        // $user->isClean(); // true 
+        // dd($user->isDirty());
+
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        //     );
+        //     $user->save();
+        //     return view('user', ['data'=>$user]);
+
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        //     );
+        //     return view('user', ['data'=>$user]);
+
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ]
+        //     );
+        //     return view('user', ['data'=>$user]);
+
+        // $user = UserModel::where('level_id', 2)->count();
+        // // dd($user);
+        // return view('user', ['data'=>$user]);
+
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data'=>$user]);
 
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data'=>$user]);
@@ -26,8 +99,8 @@ class UserController extends Controller
         // );
         // return view('user', ['data' => $user]);
 
-    // $user = UserModel::findOr(
-    //     1,
+        // $user = UserModel::findOr(
+        //     1,
         //     ['username', 'nama'],
         //     function () {
         //         abort(404);
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,6 +6,13 @@
     <body>
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
+            {{-- <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{ $data }}</td>
+            </tr> --}}
+
             <tr>
                 <th>ID</th>
                 <th>Username</th>
