47e24afc0071db067efa89325e3fc25f7604d820
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-03-09 15:24:39 +0700

    last

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+    protected $table = 'm_level';//mendefinisikan nama tabel yang dipakai oleh model ini
+    protected $primaryKey = 'level_id';//mendefinisikan primary key dari tabel yang dipakai
+    /** 
+     * @var array
+     * 
+    */
+}
