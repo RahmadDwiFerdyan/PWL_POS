b95ee0ec60a2cfdd0ac9228da8dd1418cdf525d6
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-03-15 22:00:34 +0700

    js5-praktikum2

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
 
