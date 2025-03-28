67ab90911cdb346ab1c5f18919055f4937840709
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2025-03-28 16:17:54 +0700

    login-logout auth

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; 
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
     protected $table = 'm_user';//mendefinisikan nama tabel yang dipakai oleh model ini
@@ -15,7 +16,11 @@ class UserModel extends Model
      * @var array
      * 
     */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
+
+
 
     public function level(): BelongsTo{
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
